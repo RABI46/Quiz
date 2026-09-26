68137e18bd7b7906837854b0d3a91c17f1491d77
RABI46 <59933472+RABI46@users.noreply.github.com>
2026-09-26 12:51:22 +0200

    Mise a jour du cache PWA vers v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="quiz-pse-v1";
+const CACHE="quiz-pse-v2";
 const CORE=["./","./index.html","./questions.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
